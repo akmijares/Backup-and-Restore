e2611def78794b85f02e1848c352b822a17c3c2b
Speedy905 <AK.Mijares99@gmail.com>
2020-05-21 12:55:27 -0400

    Added Dir checking and zip

--- backup.sh
+++ backup.sh
@@ -5,8 +5,23 @@ if [ $EUID != 0 ]; then
 	exit 1
 fi
 
+backupdir="/home/akmijares/backups"
+
+# Checks and creates dir if needed
+[ ! -d "$backupdir" ] && mkdir -p "$backupdir"
+
 echo "Backing up VMs"
+echo
+echo "Do not touch anything, even if it looks stuck"
 
 cd /var/lib/libvirt/images
 
+for i in {1..3}; do
+	echo
+	echo "Backing up VM$i"
+	gzip < vm$i.qcow2 > $backupdir/vm$i.qcow2.backup.gz
+	virsh dumpxml vm1 > $backupdir/vm$i.xml
+	echo "VM$i Backup completed"
+	echo;
+done
 
