94a347b3adde265bf5c7f7e270310d3e0349e4f8
Speedy905 <AK.Mijares99@gmail.com>
2020-05-21 13:15:42 -0400

    Add restore options

--- restore.sh
+++ restore.sh
@@ -9,7 +9,20 @@ fi
 read -p "Enter full dir of where the backups are located: " dir
 
 if [ -d "$dir" ]; then
-	echo "Exists"
+	echo
+	echo "Choose one of the following: "
+	echo "1 - Restore all VMs"
+	echo "2 - Restore a specific VM"
+	echo
+	read -p "Enter your choice: " ans
+	if [ "$ans" == 1 ]; then
+		echo "1"
+	elif [ "$ans" == 2 ]; then
+		echo "2"
+	else
+		echo "Unknown answer"
+		exit 1
+	fi
 else
 	echo "$dir does not exist. Please double check"
 	exit 1
