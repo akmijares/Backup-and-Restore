26bd5e357553bdc58e16481591fa5fb643d5ee69
Speedy905 <AK.Mijares99@gmail.com>
2020-05-21 13:05:25 -0400

    Added dir checking

--- restore.sh
+++ restore.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "Test"
+if [ $EUID != 0 ]; then
+	echo "Please run as root or sudo"
+	exit 1
+fi
+
+# Asks for dir and check if it exists. 
+read -p "Enter full dir of where the backups are located: " dir
+
+if [ -d "$dir" ]; then
+	echo "Exists"
+else
+	echo "$dir does not exist. Please double check"
+	exit 1
+fi
