0ed08c42d56adf63c297cfbd765fff4c2c951851
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2020-10-26 11:02:54 -0400

    Revised Script

--- backup.sh
+++ backup.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-if [ $EUID != 0 ]; then
-	echo "Please run as root or sudo"
-	exit 1
-fi
-
-# Asks for uname
-echo "This is required since you ran this as sudo/root"
-read -p "Enter your username: " uname
-
-if id "$uname" >/dev/null 2>&1; then
-	echo "Found user. Continuing."
-else
-        echo "User does not exist. Please try again"
-	exit 1
-fi
+#Adds the uname into the variable
+uname= whoami
 
 # Asks for dir and check if it exists. 
-echo ""
 echo "Note: This will be in your home directory"
 read -p "Enter the folder name of where the backups will be located: " backupdir
 
 # Checks and creates dir if needed
-[ ! -d /home/$uname/"$backupdir" ] && mkdir -p /home/$uname/"$backupdir"
+[ ! -d $HOME/"$backupdir" ] && mkdir -p $HOME/"$backupdir"
 
 echo
 echo "Choose one of the following: "
@@ -31,30 +17,39 @@ echo "2 - Backup a specific VM"
 echo "3 - Close the program"
 echo
 read -p "Enter your choice: " ans
-if [ "$ans" == 1 ]; then
-	echo "Don't touch anything, even if it looks stuck."
+
+case $ans in
+1)
+    echo "Don't touch anything, even if it looks stuck."
 	echo "You may risk corrupting your backup"
+	echo
+sudo -s <<EOF
 	cd /var/lib/libvirt/images
 	for g in *.qcow2; do
 		echo "Backing up $g"
 		name=$g
 		final=$(basename $name .qcow2)
 		gzip < $g > /home/$uname/$backupdir/$final.qcow2.backup.gz
-		virsh dumpxml $final > /home/$uname/$backupdir/$final.xml;
-		done			
-elif [ "$ans" == 2 ]; then
-	cd /var/lib/libvirt/images
+		virsh dumpxml $final > /home/$uname/$backupdir/$final.xml
+		done
+EOF
+    ;;
+
+2)
+sudo -s <<EOF
+    cd /var/lib/libvirt/images
+	echo
+	virsh list --all
 	echo
 	read -p "Enter VM to backup: " vmbackup
 	echo "Backing up $vmbackup"
 	gzip < $vmbackup.qcow2 > /home/$uname/$backupdir/$vmbackup.qcow2.backup.gz
 	virsh dumpxml $vmbackup > /home/$uname/$backupdir/$vmbackup.xml
-elif [ "$ans" == 3 ]; then
-	echo "Exiting program"
-	exit 1
-else
-	echo "Unknown answer"
-	exit 1
-fi
+EOF
+    ;;
 
-
+3|*)
+    echo "Exiting program"
+    exit 0
+    ;;
+esac
