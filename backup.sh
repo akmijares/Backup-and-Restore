3cac4bc6e194b8cf3ef582752da3e3903f931505
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2020-10-29 12:09:03 -0400

    Edit backup
    	modified:   backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run the script as sudo or root"
+  exit 1
+fi
+
+
 #Adds the uname into the variable
-uname=$USER
+echo "Note: You have to do this, otherwise, it'll default it to the root directory"
+read -p "Enter username: " user
 
 # Asks for dir and check if it exists. 
+echo
 echo "Note: This will be in your home directory"
 read -p "Enter the folder name of where the backups will be located: " backupdir
 
@@ -20,32 +28,28 @@ read -p "Enter your choice: " ans
 
 case $ans in
 1)
-    echo "Don't touch anything, even if it looks stuck."
+    	echo "Don't touch anything, even if it looks stuck."
 	echo "You may risk corrupting your backup"
 	echo
-sudo -s <<EOF
-	cd /var/lib/libvirt/images
-	for g in *.qcow2; do
+	for g in /var/lib/libvirt/images/*.qcow2;
+	do
 		echo "Backing up $g"
 		name=$g
 		final=$(basename $name .qcow2)
-		gzip < $g > /home/$uname/$backupdir/$final.qcow2.backup.gz
-		virsh dumpxml $final > /home/$uname/$backupdir/$final.xml
-		done
-EOF
+		gzip < $g > /home/$user/$backupdir/$final.qcow2.backup.gz
+		virsh dumpxml $final > /home/$user/$backupdir/$final.xml
+	done
     ;;
 
 2)
-sudo -s <<EOF
-    cd /var/lib/libvirt/images
 	echo
-	virsh list --all
+	sudo virsh list --all
 	echo
 	read -p "Enter VM to backup: " vmbackup
 	echo "Backing up $vmbackup"
-	gzip < $vmbackup.qcow2 > /home/$uname/$backupdir/$vmbackup.qcow2.backup.gz
-	virsh dumpxml $vmbackup > /home/$uname/$backupdir/$vmbackup.xml
-EOF
+	gzip < /var/lib/libvirt/images/$vmbackup.qcow2 > /home/$user/$backupdir/$vmbackup.qcow2.backup.gz
+	virsh dumpxml $vmbackup > /home/$user/$backupdir/$vmbackup.xml
+	echo "Backup of $vmbackup completed"
     ;;
 
 3|*)
